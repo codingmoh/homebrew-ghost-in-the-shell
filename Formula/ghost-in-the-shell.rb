5ac9f364ae6995dcb73cd5bada1eaa874f90c935
mo <coding.moh@gmail.com>
2025-03-30 23:14:52 +0200

    update sha256

--- Formula/ghost-in-the-shell.rb
+++ Formula/ghost-in-the-shell.rb
@@ -4,7 +4,7 @@ class GhostInTheShell < Formula
     desc "AI-powered terminal assistant that converts natural language to shell commands."
     homepage "https://github.com/codingmoh/ghost_in_the_shell"
     url "https://files.pythonhosted.org/packages/c5/de/fb89ae31371040577587ed30c323f5ee1d961db425896c84d87034f79bdb/ghost_in_the_shell-0.1.7.tar.gz"
-    sha256 "abcfa6a9d4df344d1781bc2560b5e4cdcae08b39ed303063535e7e1e926a304a"
+    sha256 "dedb2eb96f827cf2c1eb99377f6dbbad8a87c725375db9c87e0c257f31850a03"
     license "MIT"
   
     depends_on "python@3.11"
