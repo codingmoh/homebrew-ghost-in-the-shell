6b3ae55e11ecb9fbb856fb92ea753ff073a62791
mo <coding.moh@gmail.com>
2025-03-30 23:19:03 +0200

    bump

--- Formula/ghost-in-the-shell.rb
+++ Formula/ghost-in-the-shell.rb
@@ -6,7 +6,6 @@ class GhostInTheShell < Formula
     url "https://files.pythonhosted.org/packages/c5/de/fb89ae31371040577587ed30c323f5ee1d961db425896c84d87034f79bdb/ghost_in_the_shell-0.1.7.tar.gz"
     sha256 "dedb2eb96f827cf2c1eb99377f6dbbad8a87c725375db9c87e0c257f31850a03"
     license "MIT"
-  
     depends_on "python@3.11"
   
     def install
