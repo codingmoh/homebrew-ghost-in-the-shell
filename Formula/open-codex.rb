03b866614985e6adf5dceb6c58c20739f3639cbb
mo <coding.moh@gmail.com>
2025-04-23 03:03:36 +0200

    bump

--- Formula/open-codex.rb
+++ Formula/open-codex.rb
@@ -1,8 +1,8 @@
 class OpenCodex < Formula
   desc "CLI tool to generate shell commands from natural language"
   homepage "https://github.com/codingmoh/open-codex"
-  url "https://github.com/codingmoh/open-codex/releases/download/0.1.16/open-codex-macos.zip"
-  sha256 "0179d1c36c64e1fd40aecc216590ccdb0176c6ca09f6370a1a934b98c5e254f3"
+  url "https://github.com/codingmoh/open-codex/releases/download/0.1.17/open-codex-macos.zip"
+  sha256 "9c7d52b8808b3641e248860775f9cc3bcabf2bb7a577cfe12e7a9c23aae12de4"
   license "MIT"
 
   def install
