0a9a5ac7db58c8e06ed9cd79d93d8eae50ef996b
mo <coding.moh@gmail.com>
2025-04-23 02:22:34 +0200

    updated

--- Formula/open-codex.rb
+++ Formula/open-codex.rb
@@ -1,12 +1,12 @@
 class OpenCodex < Formula
   desc "CLI tool to generate shell commands from natural language"
   homepage "https://github.com/codingmoh/open-codex"
-  url "https://github.com/codingmoh/open-codex/releases/download/0.1.15/open-codex-macos.zip"
-  sha256 "88e656c1f56c2e191711cc459a1ba5d6c8544fd80fd65c764e035a51b1143403"
+  url "https://github.com/codingmoh/open-codex/releases/download/0.1.16/open-codex-macos.zip"
+  sha256 "5b195332f2bc97472fa537324b33029ab6a23eee800bd7042e655292551c117a"
   license "MIT"
 
   def install
-    bin.install "open-codex"
+    bin.install "open-codex/open-codex"
   end
 
   def post_install
