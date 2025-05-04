7d0af75e53161efad3eaa5d37aeafb41890c22fb
mo <coding.moh@gmail.com>
2025-05-04 02:09:15 +0200

    update open-codex to version 0.1.19
    Fix formula to support both Intel and Apple Silicon by using HOMEBREW_PREFIX

--- Formula/open-codex.rb
+++ Formula/open-codex.rb
@@ -1,16 +1,16 @@
 class OpenCodex < Formula
   desc     "CLI tool to generate shell commands from natural language"
   homepage "https://github.com/codingmoh/open-codex"
-  url      "https://github.com/codingmoh/open-codex/releases/download/0.1.18/open-codex-macos.zip"
-  sha256   "dfaed930e1acab739925d102ba4fd9373e70b2391bbd3910e95faa04f68ba458"
+  url      "https://github.com/codingmoh/open-codex/releases/download/0.1.19/open-codex-macos.zip"
+  sha256   "deffb2ac9d50b5e69926c5ff57eefae2072a227becd0dc0a6fba882bb2df53a1"
   license  "MIT"
 
   def install
     libexec.install Dir["*"]  
     (bin/"open-codex").write <<~EOS
-      #!/bin/bash
-      exec "/opt/homebrew/opt/open-codex/libexec/open-codex" "$@"
-      EOS
+    #!/bin/bash
+      exec "$(brew --prefix)/opt/open-codex/libexec/open-codex" "$@"
+    EOS
 
     chmod "+x", bin/"open-codex"
   end
